f8205a5f1dfaee70da3c82a7f20349543d5846be
ahmed-tarek <c.tarek2015@gmail.com>
2026-09-08 13:51:23 +0300

    feat: add isSignedin middleware for JWT authentication and protect routes

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ require("./database/database");
 // controllers
 const testJwtRoutes = require("./controllers/test-jwt");
 const authRoutes = require("./controllers/authCtrl");
+const isSignedin = require("./middlewares/isSignedin");
 
 app.use(cors());
 app.use(express.json());
@@ -18,6 +19,15 @@ app.use(logger("dev"));
 app.use("/test-jwt", testJwtRoutes);
 app.post("/auth/sign-up", authRoutes.signup);
 app.post("/auth/sign-in", authRoutes.signin);
+
+app.get("/protected", isSignedin, (req, res) => {
+  try {
+    const user = req.user;
+    res.status(200).json({ user });
+  } catch (error) {
+    res.status(500).json({ error: error.message || "errooooooorrrrrrrrr" });
+  }
+});
 
 app.listen(3000, () => {
   console.log("The express app is ready!");
