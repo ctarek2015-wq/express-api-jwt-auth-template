22ed7401222fe814b7fb96a002a58cf9bab58b0e
ahmed-tarek <c.tarek2015@gmail.com>
2026-09-08 10:52:18 +0300

    feat: add JWT authentication routes for signing and verifying tokens

--- server.js
+++ server.js
@@ -6,12 +6,15 @@ const logger = require("morgan");
 
 require("dotenv").config();
 require("./database/database");
+// controllers
+const testJwtRoutes = require("./controllers/test-jwt");
 
 app.use(cors());
 app.use(express.json());
 app.use(logger("dev"));
 
 // Routes go here
+app.use("/test-jwt", testJwtRoutes);
 
 app.listen(3000, () => {
   console.log("The express app is ready!");
